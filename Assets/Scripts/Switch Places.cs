ff0521735528dc879f13e797055cf85f30891c32
Jo6o43 <joaoribeiro141516@gmail.com>
2024-01-09 15:49:21 +0000

    Commit oku

--- Assets/Scripts/Switch Places.cs
+++ Assets/Scripts/Switch Places.cs
@@ -5,25 +5,19 @@ using UnityEngine.UI;
 
 public class SwitchPlaces : MonoBehaviour
 {
-    int image = 0;
-      // zona seca = 0
-      // zona verde = 1
-
     public Image pieceimage;
     public Sprite zonaverde;
     public Sprite zonaseca;
-
     public void imagechange()
     {
-        if (image == 0) // troca para zona verde
+        if (pieceimage == zonaseca) // troca para zona verde
         {
             pieceimage.sprite = zonaverde; 
-            image = 1;
         }
-        if (image == 1) // troca para zona seca
+        if (pieceimage == zonaverde) // troca para zona seca
         {
             pieceimage.sprite = zonaseca;
-            image = 0;
         }
     }
 }
+
