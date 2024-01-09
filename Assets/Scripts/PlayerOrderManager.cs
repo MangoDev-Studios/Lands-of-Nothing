272456ee0ed2b6d882fc3ee3a0ff31dcd1753ba8
GONCAVF <goncalo37865@gmail.com>
2024-01-09 15:37:35 +0000

    Current Player (not working)

--- Assets/Scripts/PlayerOrderManager.cs
+++ Assets/Scripts/PlayerOrderManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public class PlayerOrderManager : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class PlayerOrderManager : MonoBehaviour
 
     private PlayerRoll[] playerRolls = new PlayerRoll[4];
     private bool playerOrderDetermined = false;
+    private int[] playerOrder;
+
+    private Dictionary<int, GameObject> playerDictionary = new Dictionary<int, GameObject>();
 
     public void RecordDiceRoll(int playerId, int rollResult)
     {
@@ -38,13 +42,38 @@ public class PlayerOrderManager : MonoBehaviour
         {
             Array.Sort(playerRolls, (p1, p2) => p2.rollValue.CompareTo(p1.rollValue));
 
-            Debug.Log("Player order: " + string.Join(", ", Array.ConvertAll(playerRolls, p => p.playerId)));
+            playerOrder = Array.ConvertAll(playerRolls, p => p.playerId);
+
+            Debug.Log("Player order: " + string.Join(", ", playerOrder));
 
             playerOrderDetermined = true;
+
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+            foreach (var player in players)
+            {
+                int playerId = player.GetComponent<PlayerController>().PlayerID;
+                playerDictionary.Add(playerId, player);
+            }
         }
         else
         {
             Debug.LogWarning("Player order already determined.");
         }
+    }
+
+    public int GetCurrentPlayerTurn()
+    {
+        if (playerOrderDetermined)
+        {
+            int currentPlayerId = playerOrder[0];
+
+            if (playerDictionary.TryGetValue(currentPlayerId, out GameObject currentPlayerObject))
+            {
+                return currentPlayerObject.GetComponent<PlayerController>().PlayerID + 1;
+            }
+        }
+
+        return -1;
     }
 }
